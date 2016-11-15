7752593e373b60c537d80d2be30fa113988fc346
xwp_fullstack <xwp_fullstack@163.com>
2016-11-15 03:07:07 -0800

    update

--- sayhello/views.py
+++ sayhello/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def sayhello(request):
-    return HttpResponse('hi, say hello world!')
+    return HttpResponse('hi, say hello world itcast!')
