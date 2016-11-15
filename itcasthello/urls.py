ef3d1da1e6c17e2b5e38e70432f175229ddf1f5b
xwp_fullstack <xwp_fullstack@163.com>
2016-11-15 02:51:05 -0800

    add sayhello

--- itcasthello/urls.py
+++ itcasthello/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from sayhello import views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.sayhello),
 ]
